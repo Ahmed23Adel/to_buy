2231929bce634b8b6c16542e8e21cf61e43e1687
aaahhhmmmeeeddd <AH644Hu@ya>
2020-09-15 21:16:21 +0200

    solving not creating activities problem

---
+++ app/src/main/java/com/wellmax8/tobuy/View/add_category.java
@@ -0,0 +1,16 @@
+package com.wellmax8.tobuy.View;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class add_category extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
