db21fa5cc3154a1669a02cc9bfefbec47101e980
aaahhhmmmeeeddd <AH644Hu@ya>
2020-09-16 23:27:22 +0200

    hiding add image when there is data

--- app/src/main/java/com/wellmax8/tobuy/View/categories.java
+++ app/src/main/java/com/wellmax8/tobuy/View/categories.java
@@ -9,10 +9,12 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.wellmax8.tobuy.R;
+import com.wellmax8.tobuy.ViewModel.VM_categories;
 import com.wellmax8.tobuy.cursor_adapter_categories;
 
 public class categories extends AppCompatActivity {
@@ -20,13 +22,22 @@ public class categories extends AppCompatActivity {
     private FloatingActionButton add_category;
     private RecyclerView recyclerView;
     private TextView textView_add_category;
-    cursor_adapter_categories mCursor_adapter_categories;
+    private VM_categories VM;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.categories);
         instantiateViews();
         setActionOnButtons();
+        VM=new ViewModelProvider(this).get(VM_categories.class);
+        VM.setContext(this);
+        VM.getCategoriesOrderedCreatedAtDESC().observe(this,categories -> {
+            if (!categories.isEmpty()){
+                categoriesNotEmpty();
+            }
+        });
+
     }
 
     private void instantiateViews(){
@@ -52,6 +63,13 @@ public class categories extends AppCompatActivity {
             }
         };
         return onClickListener;
+    }
+
+    private void categoriesNotEmpty(){
+        imageView_add_category.setVisibility(View.GONE);
+        textView_add_category.setVisibility(View.GONE);
+        recyclerView.setVisibility(View.VISIBLE);
+
     }
 
 
---
+++ app/src/main/java/com/wellmax8/tobuy/ViewModel/VM_categories.java
@@ -0,0 +1,32 @@
+package com.wellmax8.tobuy.ViewModel;
+
+import android.content.Context;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.wellmax8.tobuy.DTO.category;
+import com.wellmax8.tobuy.ROOM.category.repo_category;
+
+import java.util.List;
+
+public class VM_categories extends ViewModel {
+
+    private Context context;
+    private repo_category repo;
+
+    public VM_categories() {
+
+    }
+    public void setContext(Context context) {
+        this.context = context;
+        repo=new repo_category(context);
+    }
+
+    public LiveData<List<category>> getCategoriesOrderedCreatedAtDESC(){
+        return repo.getCategoriesOrderedCreatedAtDESC();
+    }
+
+
+
+}
