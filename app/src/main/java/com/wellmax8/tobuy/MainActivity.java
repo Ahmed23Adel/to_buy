5391819dd124d57988087d1cdc1333875dbfb6a3
aaahhhmmmeeeddd <AH644Hu@ya>
2020-09-16 23:18:29 +0200

    creating categories activity

--- app/src/main/java/com/wellmax8/tobuy/MainActivity.java
+++ app/src/main/java/com/wellmax8/tobuy/MainActivity.java
@@ -1,44 +1,25 @@
 package com.wellmax8.tobuy;
 
-import androidx.annotation.MainThread;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.wellmax8.tobuy.DTO.category;
-import com.wellmax8.tobuy.DTO.category_builder;
 import com.wellmax8.tobuy.ViewModel.VM_add_category;
-import com.wellmax8.tobuy.data.toBuyDbHelper;
 
 import android.app.LoaderManager;
-import android.content.ContentValues;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.wellmax8.tobuy.data.toBuyContract;
 import com.wellmax8.tobuy.data.toBuyContract.categories_entry;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
-import java.util.List;
-import java.util.logging.Logger;
-import androidx.lifecycle.ViewModelProviders;
 
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
@@ -54,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.categories);
 
         VM_add_category vm_add_category=new ViewModelProvider(this).get(VM_add_category.class);
         vm_add_category.setContext(this);
---
+++ app/src/main/java/com/wellmax8/tobuy/View/categories.java
@@ -0,0 +1,58 @@
+package com.wellmax8.tobuy.View;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.wellmax8.tobuy.R;
+import com.wellmax8.tobuy.cursor_adapter_categories;
+
+public class categories extends AppCompatActivity {
+    private ImageView imageView_add_category;
+    private FloatingActionButton add_category;
+    private RecyclerView recyclerView;
+    private TextView textView_add_category;
+    cursor_adapter_categories mCursor_adapter_categories;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.categories);
+        instantiateViews();
+        setActionOnButtons();
+    }
+
+    private void instantiateViews(){
+        imageView_add_category = findViewById(R.id.main_imageView_add);
+        add_category = findViewById(R.id.main_fab);
+        recyclerView = findViewById(R.id.main_listView);
+        textView_add_category = findViewById(R.id.main_textView_add);
+    }
+
+    private void setActionOnButtons(){
+        imageView_add_category.setOnClickListener(getActionToAddCategory());
+        textView_add_category.setOnClickListener(getActionToAddCategory());
+        add_category.setOnClickListener(getActionToAddCategory());
+
+    }
+
+    private OnClickListener getActionToAddCategory(){
+        OnClickListener onClickListener=new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(categories.this,add_category.class);
+                startActivity(intent);
+            }
+        };
+        return onClickListener;
+    }
+
+
+}
