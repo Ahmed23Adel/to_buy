ee3d5b411b6e2cab1df22ee5fa9fcb9896bc99aa
aaahhhmmmeeeddd <AH644Hu@ya>
2020-09-18 21:43:54 +0200

    get sharedPref working

--- app/src/main/java/com/wellmax8/tobuy/Fragments/viewQuilt.java
+++ app/src/main/java/com/wellmax8/tobuy/Fragments/viewQuilt.java
@@ -1,23 +1,35 @@
 package com.wellmax8.tobuy.Fragments;
 
-import androidx.lifecycle.ViewModelProviders;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.wellmax8.tobuy.R;
 import com.wellmax8.tobuy.ViewModel.VM_Frag_view_quilt;
 
 public class viewQuilt extends Fragment {
 
-    private VM_Frag_view_quilt mViewModel;
+    private LinearLayout layoutLargeStyle;
+    private LinearLayout layoutSmallStyle;
+    private ImageView largeStyle;
+    private ImageView smallStyle;
+    private VM_Frag_view_quilt VM;
 
     public static viewQuilt newInstance() {
         return new viewQuilt();
@@ -26,14 +38,61 @@ public class viewQuilt extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.view_quilt_fragment, container, false);
+        VM = new ViewModelProvider(this).get(VM_Frag_view_quilt.class);
+
+        View view = inflater.inflate(R.layout.view_quilt_fragment, container, false);
+        layoutLargeStyle = view.findViewById(R.id.layout_largeStyle);
+        layoutSmallStyle = view.findViewById(R.id.layout_smallStyle);
+        largeStyle = view.findViewById(R.id.largeStyle);
+        smallStyle = view.findViewById(R.id.smallStyle);
+
+        layoutLargeStyle.setOnClickListener(getListener(true));
+        largeStyle.setOnClickListener(getListener(true));
+        layoutSmallStyle.setOnClickListener(getListener(false));
+        smallStyle.setOnClickListener(getListener(false));
+
+        isCategoriesViewQuiltLarge();
+        return view;
     }
+
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(VM_Frag_view_quilt.class);
+
         // TODO: Use the ViewModel
     }
 
+    private OnClickListener getListener(boolean large){
+        OnClickListener onClickListener=new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (large){
+                    VM.setViewQuiltToLarge(getContext());
+                    switchToLarge();
+                }else{
+                    VM.setViewQuiltToSmall(getContext());
+                    switchToSmall();
+                }
+            }
+        };
+        return onClickListener;
+    }
+
+    private void switchToLarge(){
+        layoutLargeStyle.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.grey));
+        layoutSmallStyle.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
+    }
+    private void switchToSmall(){
+        layoutLargeStyle.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.white));
+        layoutSmallStyle.setBackgroundColor(ContextCompat.getColor(getContext(), R.color.grey));
+    }
+
+    private void isCategoriesViewQuiltLarge() {
+        if (VM.isCategoriesViewQuiltLarge(getContext())){
+            switchToLarge();
+        }else{
+            switchToSmall();
+        }
+    }
 }
--- app/src/main/java/com/wellmax8/tobuy/ViewModel/VM_Frag_view_quilt.java
+++ app/src/main/java/com/wellmax8/tobuy/ViewModel/VM_Frag_view_quilt.java
@@ -2,6 +2,7 @@ package com.wellmax8.tobuy.ViewModel;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
@@ -10,25 +11,33 @@ import com.wellmax8.tobuy.R;
 public class VM_Frag_view_quilt extends ViewModel {
     // TODO: Implement the ViewModel
 
-    public VM_Frag_view_quilt(Context context) {
+    public VM_Frag_view_quilt() {
 
     }
 
+    public boolean isCategoriesViewQuiltLarge(Context context){
+        SharedPreferences sharedPreferences= context.getSharedPreferences("com_wellmax8_tobuy",Context.MODE_PRIVATE);
+        boolean isLarge=sharedPreferences.getBoolean(context.getString(R.string.keyCategories_largeStyle),true);
+        boolean isSmall=sharedPreferences.getBoolean(context.getString(R.string.keyCategories_smallStyle),false);
+        return isLarge;
+    }
+
     public void setViewQuiltToLarge(Context context){
-        SharedPreferences sharedPref = context.getSharedPreferences(context.getString(R.string.keyCategories_largeStyle)
+        SharedPreferences sharedPref = context.getSharedPreferences("com_wellmax8_tobuy"
                 ,Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPref.edit();
         editor.putBoolean(context.getString(R.string.keyCategories_largeStyle),true);
         editor.putBoolean(context.getString(R.string.keyCategories_smallStyle),false);
-        editor.commit();
+        editor.apply();
     }
 
     public void setViewQuiltToSmall(Context context){
-        SharedPreferences sharedPref=context.getSharedPreferences(context.getString(R.string.keyCategories_smallStyle)
+        SharedPreferences sharedPref=context.getSharedPreferences("com_wellmax8_tobuy"
                 ,Context.MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPref.edit();
         editor.putBoolean(context.getString(R.string.keyCategories_largeStyle),false);
         editor.putBoolean(context.getString(R.string.keyCategories_smallStyle),true);
+        editor.apply();
     }
 
 }
